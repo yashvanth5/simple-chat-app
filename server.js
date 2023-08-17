ab0453bf71576b77cc67a8ef0689c058b094ad82
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2023-08-17 19:18:57 +0530

    refactor made some changes in code

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const port = process.env.PORT || 2500;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
