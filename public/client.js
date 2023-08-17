74d98f62c209b0fb1a1da67b6823da448fbb93ca
yashvanth5 <116138513+yashvanth5@users.noreply.github.com>
2023-08-17 18:31:16 +0530

    feat : added code to our chat

---
+++ public/client.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io("http://localhost:2500"); // Connect to the server
+
+  const sendButton = document.getElementById("button");
+  const inputField = document.getElementById("messageInput");
+  const chatMessages = document.querySelector(".chat-messages");
+
+  sendButton.addEventListener("click", () => {
+    const message = inputField.value;
+    if (message) {
+      socket.emit("message", message); // Send the message to the server
+      inputField.value = "";
+    }
+  });
+
+  socket.on("message", (message) => {
+    const newMessage = document.createElement("div");
+    newMessage.className = "message incoming";
+    newMessage.textContent = message;
+    chatMessages.appendChild(newMessage);
+  });
+});
---
+++ public/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("message", (message) => {
+    io.emit("message", message); // Broadcast the message to all connected clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+const port = process.env.PORT || 2500;
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
